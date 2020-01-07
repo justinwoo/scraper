5f456e00dad31ca9615d9d93e9aff5970afcf03d
justinwoo <moomoowoo@gmail.com>
2020-01-07 21:56:10 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
+
+in
+pkgs.stdenv.mkDerivation rec {
+  name = "nixpkgs-fmt";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/justinwoo/scraper/releases/download/2019-12-29/scraper";
+    sha256 = "1x8s3qj611lnf4djzhird0mxmrrj5kq319czfwbplpnmhp3z25wg";
+  };
+
+  buildInputs = [ pkgs.makeWrapper ];
+
+  dontStrip = true;
+
+  libPath = pkgs.lib.makeLibraryPath [ pkgs.glibc ];
+
+  unpackPhase = ''
+    mkdir -p $out/bin
+    TARGET=$out/bin/scraper
+
+    cp $src $TARGET
+    chmod +x $TARGET
+
+    patchelf $TARGET \
+      --interpreter ${dynamic-linker} \
+      --set-rpath ${libPath}
+  '';
+
+  dontInstall = true;
+}
